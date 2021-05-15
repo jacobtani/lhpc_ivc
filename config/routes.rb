2a21269716b4eb005c733b54e0f4059bf2529cc4
Tania Jacob <tania@abletech.nz>
2021-05-15 21:38:39 +1200

    Add admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'application#page_not_found'
 
+  namespace :admin do
     devise_for :users, only: [:sessions, :passwords, :registrations], :controllers => { :sessions => "admin/sessions", passwords: "admin/passwords", registrations: "admin/registrations" }
     resources :users
+  end
 end
