59f9de9747ba5c6e5b0930e25bfcc493ff080508
Tania Jacob <tania@abletech.nz>
2021-05-15 21:38:15 +1200

    Add page not found as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'application#page_not_found'
+
     devise_for :users, only: [:sessions, :passwords, :registrations], :controllers => { :sessions => "admin/sessions", passwords: "admin/passwords", registrations: "admin/registrations" }
     resources :users
 end
