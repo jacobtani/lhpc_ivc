0cbe329aefd95c529c3098824ca324bec1c6233b
Tania Jacob <tania@abletech.nz>
2021-05-16 17:05:53 +1200

    Add members and invoices

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   namespace :admin do
     devise_for :users, only: [:sessions, :passwords, :registrations], :controllers => { :sessions => "admin/sessions", passwords: "admin/passwords", registrations: "admin/registrations" }
     resources :users
+    resources :members do
+      member do
+        get 'children'
+      end
+      resources :invoices
+    end
   end
 end
